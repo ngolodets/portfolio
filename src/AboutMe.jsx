a060d813a696ad6f049fe1cae2b3b2304c47a201
ngolodets <nina.golodets@gmail.com>
2019-11-16 15:38:59 -0800

    added email form

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -15,7 +15,7 @@ function AboutMe() {
         Hard working, resourceful, analytical and detail-driven, I have experience working with cross-functional teams and understand the importance of sticking to deadlines to produce deliverables while working under pressure.
         With a background in chemical engineering, I know how to manage multiple projects with competing deadlines to produce timely and effective deliverables that meet and exceed expectations.
       </p>
-      <p>Please feel free to email me at <span>nina.golodets@gmail.com</span></p>
+      <p>Please feel free to email me at <a id='email' href="mailto:nina.golodets@gmail.com">nina.golodets@gmail.com</a></p>
     </div>
   )
 }
